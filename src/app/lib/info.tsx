27926ca8376a7dde881a9d23616347d2375d55be
Christian <cphvb91@gmail.com>
2023-03-12 18:10:44 +0100

    initial commit

---
+++ src/app/lib/info.tsx
@@ -0,0 +1,21 @@
+import me from './me.jpg'
+
+export const name = 'Christian von Bredow';
+
+export const avatar = me
+
+export const about = () => {
+  return (
+    <>
+      Hello. I'm Christian. I'm a <b>web developer</b> and I build things for a living and for fun.
+    </>
+  );
+};
+export const bio = () => {
+  return (
+    <>
+      I have a diverse background. I worked previously in digital communications
+      and academia. Now I specialise in building beautiful web-based experiences.
+    </>
+  )
+};
