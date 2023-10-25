db92f2ddf8c2ea27ece2f8b805be7f776b8e8d58
Christian <cphvb91@gmail.com>
2023-10-25 12:53:31 +0100

    updated info section

--- src/app/lib/info.tsx
+++ src/app/lib/info.tsx
@@ -1,6 +1,6 @@
 import me from './me.jpg'
 
-export const name = 'Christian von Bredow';
+export const name = 'Christian von Bredow'
 
 export const avatar = me
 
@@ -10,19 +10,20 @@ export const about = () => {
       Hello. I am Christian. I am a{' '}
       <span className='text-[#ecb365]'>
         <b>web developer</b>
-      </span>{' '} and I specialise in building beautiful web-based experiences.
+      </span>{' '}
+      and I specialise in building beautiful web-based experiences.
     </>
   )
-};
+}
 export const bio = () => {
   return (
     <>
       <span>
-        I am a full stack developer based in Berlin and I combine my background
-        in digital communications and political science to create diverse digtal
-        experiences . As a self-taught programmer, my unyielding determination
-        and ability to work independently showcase my passion for
-        problem-solving. <br></br>
+        I am an experienced full stack developer and I work with a wide variety
+        of web technologies. This portfolio is a combination of some professional work and some work I have built for fun. I am always seeking to expand my repotoire and stay abreast
+        of the cutting edge advancements the web has to offer. It is such and
+        exciting time to be working in this field and the best is yet to come!
+        <br></br>
         <br></br> I enjoy working with a wide array of web technologies,
         consistently seeking opportunities to expand my ever-growing knowledge
         base. Infused with a thirst for learning, I truly embrace what it means
@@ -32,14 +33,14 @@ export const bio = () => {
       </span>
       <span> </span>
       <span>
-        I am currently working freelance and am always looking for new
-        opportunities. If you have a project that you would like to discuss,
-        please feel free to get in{' '}
+        I am currently working for Nutri-Tech Solutions, an agri-tech company
+        based in Australia. I am always looking for new ways of collaborating.
+        If you have a project that you would like to discuss, please feel free
+        to get in{' '}
         <a className='font-bold text-[#ecb365]' href='/contact'>
           touch.
         </a>
-        
       </span>
     </>
   )
-};
+}
